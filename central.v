bb36c12e37d76abdb933188a6f90ee94f9fb6d79
Bruno Lima <bg.lima@hotmail.com>
2016-11-08 17:16:07 -0200

    Testing fetch/decode initial sequence

--- central.v
+++ central.v
@@ -1,14 +1,44 @@
-module central (op, clk, clr, mux_sum, mux_y, we, en_fetch, r);
+module central (op, clk, clr, mux_sum, mux_y, we, en_fetch, r, inst_en);
 input [2:0] op;
 input clk, clr;
 output 	[1:0] mux_sum;
-reg		[1:0]mux_sum;
+reg		[1:0] mux_sum;
+output reg [5:0] inst_en;	//  PC_EN, INST_EN, ADDR_EN, Y_EN, OP_EN, X_EN
 output reg mux_y;
 output reg we; 
 output reg en_fetch;
 output reg r;
 
+reg [1:0] inst_state;
+reg [1:0] inst_next_state;	
+reg	initOk = 0;
+
 always @ (posedge clk) begin
+	inst_state <= inst_next_state;
+	if (!initOk)
+		case (next_state)
+			2'b00 : inst_en <= 6'b000000;
+			2'b01 : inst_en <= 6'b100000; 
+			2'b10 : inst_en <= 6'b110000;
+			2'b11 : inst_en <= 6'b111111;
+		endcase
+end
+
+always begin
+	// Init sequence 
+	case ( int_state )
+		2'b00 : next_state = 2'b01;
+		2'b01 : next_state = 2'b10;
+		2'b10 : next_state = 2'b11;
+		2'b11 : begin
+			next_state = 2'b00;
+			initOk = 1;
+		end
+	endcase
+end
+
+/* always @ (posedge clk) begin
+
 
 		if (op == 3'b000)
 			begin
@@ -43,5 +73,6 @@ always @ (posedge clk) begin
 			
 			end
 
-	end
+
+	end*/
 endmodule
